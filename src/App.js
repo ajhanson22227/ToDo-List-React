20c2af20483c1be648382d6584e5315e9936c3be
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-09 15:03:20 -0600

    Add icons for html tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+/*
+  Project will be an object with
+  {
+    title: text,
+    tasks: Array
+  }
+*/
 
 const App = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    let projArr = [];
+    
+  })
+
   return (
     <div>
       <p>Hello</p>
