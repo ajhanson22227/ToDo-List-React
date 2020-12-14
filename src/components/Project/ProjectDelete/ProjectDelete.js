3b439e475585aa8c5da2253c3c8665fc37dc0111
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-14 13:43:16 -0600

    Add handle submit to call project delete API

--- src/components/Project/ProjectDelete/ProjectDelete.js
+++ src/components/Project/ProjectDelete/ProjectDelete.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import { deleteProject } from "../../../api/fetchCalls";
 
 const ProjectDelete = () => {
   let history = useHistory();
+  let { id } = useParams();
 
-  const handleCancel = (event) => {
+  const handleCancel = () => {
     history.push("/project");
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await Promise.resolve(deleteProject(id));
+    setTimeout(() => {
+      history.push("/project");
+    }, 1000);
   };
 
   return (
     <div className="delete-form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Are you Sure you want to delete this project?</label>
         <br />
         <input type="submit" value="Delete" />
