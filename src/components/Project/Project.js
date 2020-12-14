9525a1a0f3c291908769307bb33e41d4357343d1
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-14 11:49:11 -0600

    Modify project container to have View and Delete links

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Project.css";
 
 const Project = ({ project }) => {
   return (
     <div className="project">
-      <p className="project-title">{project.title}</p>
-      <p className="project-description">{project.description}</p>
-      <p className="project-tasks">{project.tasks.length} Tasks</p>
+      <div className="project-information">
+        <p className="project-title">{project.title}</p>
+        <p className="project-description">{project.description}</p>
+        <p className="project-tasks">{project.tasks.length} Tasks</p>
+      </div>
+      <div className="project-link">
+        <Link to={`/project/${project._id}/view`}>View</Link>
+      </div>
+      <div className="project-link">
+        <Link to={`/project/${project._id}/delete`}>Delete</Link>
+      </div>
     </div>
   );
 };
