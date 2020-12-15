2fa31c6bcea7701be65d45e94cc0dbfd9492f64e
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-15 11:57:34 -0600

    Add component form to create task for project

--- src/components/Task/TaskCreate/TaskCreate.js
+++ src/components/Task/TaskCreate/TaskCreate.js
@@ -1,14 +1,82 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import { createTask } from "../../../api/fetchCalls";
 
 const TaskCreate = () => {
   const { projid } = useParams();
+  let history = useHistory();
   const [busy, setBusy] = useState(true);
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+
+  const handleDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handlePriority = (event) => {
+    setPriority(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const task = {
+      description: description,
+      priority: priority,
+      project: projid,
+    };
+    await Promise.resolve(createTask(task));
+    setTimeout(() => history.push("/project"), 1000);
+  };
 
   return (
     <div>
-      <p>Hello</p>
-      <p>From task create</p>
+      <p>Create a Task</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Description
+          <br />
+          <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={handleDescription}
+          />
+        </label>
+        <br />
+        <label onChange={handlePriority}>
+          Priority
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="priority"
+              value="Low"
+              //onChange={handlePriority}
+            />
+            Low
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="priority"
+              value="Medium"
+              //onChange={handlePriority}
+            />
+            Medium
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="priority"
+              value="High"
+              //onChange={handlePriority}
+            />
+            High
+          </label>
+        </label>
+        <br />
+        <input type="submit" value="Add" />
+      </form>
     </div>
   );
 };
