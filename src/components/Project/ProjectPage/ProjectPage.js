9b75c0bfb55e58092e0c1a08dd1bb9d553c1d437
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-16 14:36:50 -0600

    Add styles and classes to project page

--- src/components/Project/ProjectPage/ProjectPage.js
+++ src/components/Project/ProjectPage/ProjectPage.js
@@ -8,15 +8,23 @@ const ProjectPage = ({ user }) => {
   const projects = useProjects(user, setLoading);
   return (
     <div>
+      <div className="top-bar">
+        <p>Too-Doo</p>
+      </div>
       {loading ? (
         <p>is loading, hold on</p>
       ) : (
-        <div>
-          <p>Hello {user.username}</p>
-          <p>You have {projects ? projects.length : 0} projects</p>
-          <p>
-            <Link to={`/project/create`}>Create New Project</Link>
-          </p>
+        <div className="project-page-container">
+          <div className="header">
+            <div className="user-info">
+              <p>Hello {user.username}</p>
+              <p>You have {projects ? projects.length : 0} projects</p>
+            </div>
+            <div className="create-project-button">
+              <Link to={`/project/create`}>Create New Project</Link>
+            </div>
+          </div>
+
           <div className="project-container">{projects}</div>
         </div>
       )}
