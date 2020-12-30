e73fd58c098d1f0a20b539a995d933054e41e638
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-30 15:19:12 -0600

    Add section classess to the tasks for each half

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "./Task.css";
 
 const Task = ({ task }) => {
   return (
-    <div className="task">
-      <div></div>
-      <div>{task.description}</div>
-      <div className={`${task.priority}`}>{task.priority} Priority</div>
-      <div>Delete</div>
+    <div className=" task df-row">
+      <div className="section-1 df-row">
+        <div className="task-checkbox"></div>
+        <div>{task.description}</div>
+      </div>
+      <div className="section-2 df-row">
+        <div className={`${task.priority} priority`}>
+          {task.priority} Priority
+        </div>
+        <div>Delete</div>
+      </div>
     </div>
   );
 };
