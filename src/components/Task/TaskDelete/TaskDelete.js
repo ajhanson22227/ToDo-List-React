1cd4624bd763324fe88b755116ee6269b98a835b
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-31 11:38:25 -0600

    Create and add TaskDelete component

---
+++ src/components/Task/TaskDelete/TaskDelete.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
+import { deleteTask } from "../../../api/fetchCalls";
+
+const TaskDelete = ({ taskToDelete, setTaskDelete }) => {
+  let history = useHistory();
+  let { url } = useRouteMatch();
+  let id = taskToDelete._id;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await Promise.resolve(deleteTask(id));
+    setTimeout(() => {
+      setTaskDelete(false);
+      history.push(url);
+    });
+  };
+
+  return (
+    <div className="delete-modal">
+      <div className="delete-modal-content">
+        <form onSubmit={handleSubmit}>
+          <label>Are you Sure you want to delete this task?</label>
+          <p>
+            <span>{taskToDelete.description}</span>
+          </p>
+          <br />
+          <input type="submit" value="Delete" />
+          <input
+            type="button"
+            value="Cancel"
+            onClick={() => setTaskDelete(false)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TaskDelete;
