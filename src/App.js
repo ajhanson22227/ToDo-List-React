aefd0a6b2f921df6ed416cfe955675e4966de382
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-07 14:43:47 -0600

    Remove loading state from App component Loading refreshed page and lost child states values

--- src/App.js
+++ src/App.js
@@ -11,38 +11,25 @@ import "./App.css";
 const App = () => {
   const [user, setUser] = useState(fetchUserStorage());
   const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(false);
 
   return (
-    <div>
-      {loading ? (
-        <p>Loading..</p>
-      ) : (
-        <Switch>
-          <Route
-            path="/user/signin"
-            exact
-            component={() => (
-              <SignInPage setUser={setUser} setLoading={setLoading} />
-            )}
-          />
-          <Route
-            path="/user/signup"
-            exact
-            component={() => (
-              <SignUpPage setUser={setUser} setLoading={setLoading} />
-            )}
-          />
-          <Route
-            path="/project"
-            component={() => (
-              <ProjectMain user={user} setProjects={setProjects} />
-            )}
-          />
-          <Redirect from="/" to="/user/signin" />
-        </Switch>
-      )}
-    </div>
+    <Switch>
+      <Route
+        path="/user/signin"
+        exact
+        component={() => <SignInPage setUser={setUser} />}
+      />
+      <Route
+        path="/user/signup"
+        exact
+        component={() => <SignUpPage setUser={setUser} />}
+      />
+      <Route
+        path="/project"
+        component={() => <ProjectMain user={user} setProjects={setProjects} />}
+      />
+      <Redirect from="/" to="/user/signin" />
+    </Switch>
   );
 };
 
