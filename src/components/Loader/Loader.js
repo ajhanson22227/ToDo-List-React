132e441ad33ed00971ab67aab93030cb9350b8ae
ajhanson22227 <ajhanson22227@gmail.com>
2021-01-07 15:16:02 -0600

    Wrap wheel in an overlay div to display background:

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Loader.css";
 
 const Loader = () => {
-  return <div className="loading-wheel"></div>;
+  return (
+    <div className="loading-overlay">
+      <div className="loading-wheel"></div>
+    </div>
+  );
 };
 
 export default Loader;
